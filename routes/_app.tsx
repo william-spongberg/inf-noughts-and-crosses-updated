c9f873b08a095e102b4c6b427f0a8548cd5b0826
William Spongberg <william.spongberg@outlook.com>
2025-03-13 16:56:05 +1100

    Fixed title

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>nothofagus</title>
+        <title>Infinite Noughts and Crosses</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,15 +1,9 @@
 import * as Layout from "../components/Layout.tsx";
-import * as Text from "../components/Text.tsx";
 import InfGrid from "../islands/InfGrid.tsx";
-import { Head } from "$fresh/runtime.ts";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Infinite Noughts and Crosses</title>
-      </Head>
-
       <Layout.Page>
         <InfGrid />
       </Layout.Page>
